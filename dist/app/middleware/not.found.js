962164716140e51154f8423d123be8b1f74ba0d5
strongfornt <au0038173@gmail.com>
2026-03-08 19:56:02 +0600

    remove dist

--- dist/app/middleware/not.found.js
+++
@@ -1,9 +0,0 @@
-import { StatusCodes } from 'http-status-codes';
-const notFound = (req, res, next) => {
-    res.status(StatusCodes.NOT_FOUND).json({
-        success: false,
-        message: 'Not Found Api',
-        error: '',
-    });
-};
-export default notFound;
--- dist/app/middleware/validationMiddleware.js
+++
@@ -1,8 +0,0 @@
-import catchAsync from "../utils/catch-async";
-export const validationMiddleWare = (schema) => {
-    return catchAsync(async (req, res, next) => {
-        // const { data } = req.body;
-        await schema.parseAsync(req?.body);
-        next();
-    });
-};
